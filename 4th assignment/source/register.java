189bc4267744843eb84bab64ac2b581e10930594
sulochanamulpuri <31327190+sulochanamulpuri@users.noreply.github.com>
2017-09-20 23:40:44 -0500

    sfinal

--- 4th assignment/source/register.java
+++ 4th assignment/source/register.java
@@ -1,7 +1,10 @@
-package com.example.snepi.lb4;
+package com.example.snepi.firstassignment;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class register extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        final Button contact = (Button) findViewById(R.id.button4);
+        contact.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent act1 = new Intent(view.getContext(),login.class);
+                startActivity(act1);
+
+            }
+        });
     }
 }
